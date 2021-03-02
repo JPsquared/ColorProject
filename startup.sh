8d5ba2761b930982abf809aeb3c2aee7120fa882
JPsquared <jpjpost1@gmail.com>
2021-03-02 17:01:51 -0500

    Mess with startup.sh. Remove ls call from first tab execution

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gnome-terminal --tab --command="bash -c 'cd ~/ColorProject; ls; $SHELL'" \
+gnome-terminal --tab --command="bash -c 'cd ~/ColorProject; $SHELL'" \
                --tab --command="bash -c 'cd ~; sleep 5; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/administrator/roblab_with_balloons.world; $SHELL'" \
                --tab --command="bash -c 'cd ~; sleep 6; roslaunch continuous_driver continuous_driver.launch; $SHELL'"
 
