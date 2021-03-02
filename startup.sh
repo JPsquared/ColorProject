94d2a349b79b10cb1c5ed73270e90f77ef565fe7
JPsquared <jpjpost1@gmail.com>
2021-03-02 16:04:46 -0500

    Working on startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-gnome-terminal --tab --title="Shell" --command="bash -c 'cd ~; ls; $SHELL'" --tab --title="Gazebo" --command="bash -c 'cd ~; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/administrator/roblab_with_balloons.world; $SHELL'" --tab --title="Driver" --command="bash -c 'cd ~; roslaunch continuous_driver continuous_driver.launch; $SHELL'"
+gnome-terminal --tab --command="bash -c 'cd ~/ColorProject; ls; $SHELL'" \
+               --tab --command="bash -c 'cd ~; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/administrator/roblab_with_balloons.world; $SHELL'" \
+               --tab --command="bash -c 'sleep 5; cd ~; roslaunch continuous_driver continuous_driver.launch; $SHELL'"
