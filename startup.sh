95e8139a63356baab14f996d135cc7b07149d3b2
JPsquared <jpjpost1@gmail.com>
2021-03-02 16:25:07 -0500

    Add sleep commands to startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 gnome-terminal --tab --command="bash -c 'cd ~/ColorProject; ls; $SHELL'" \
-               --tab --command="bash -c 'cd ~; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/administrator/roblab_with_balloons.world; $SHELL'" \
-               --tab --command="bash -c 'sleep 5; cd ~; roslaunch continuous_driver continuous_driver.launch; $SHELL'"
+               --tab --command="bash -c 'cd ~; sleep 5; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/administrator/roblab_with_balloons.world; $SHELL'" \
+               --tab --command="bash -c 'cd ~; sleep 6; roslaunch continuous_driver continuous_driver.launch; $SHELL'"
