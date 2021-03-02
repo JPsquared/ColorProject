623d45cce0bad54f72b51e4b59e99590ee782b7d
JPsquared <jpjpost1@gmail.com>
2021-03-02 18:54:49 -0500

    Create imageGatherer.py

--- imageGatherer.py
+++ imageGatherer.py
@@ -1,1 +1,14 @@
 import rospy
+from time import sleep
+from numpy import pi
+from turtleAPI import robot
+
+
+if __name__ == "__main__":
+    rate = rospy.Rate(10)
+    rbt = robot()
+    startAngle = rbt.getAngle()
+    # rbt.drive(.2, 0)
+    sleep(1)
+    currentAngle = rbt.getAngle()
+    print(currentAngle)
