dc01700a0f167534f44188e6f7f025ec88843de6
JPsquared <jpjpost1@gmail.com>
2021-03-02 15:58:39 -0500

    Working on startup.sh

--- startup.sh
+++ startup.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-echo "$SHELL"
-
-bash roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/administrator/roblab_with_balloons.world &
-
-echo "Gazebo launched?"
-read REPLY
-
-bash roslaunch continuous_driver continuous_driver.launch &
-
-echo "Continuous driver launched?"
-read REPLY
+gnome-terminal --tab --title="Shell" --command="bash -c 'cd ~; ls; $SHELL'" --tab --title="Gazebo" --command="bash -c 'cd ~; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/administrator/roblab_with_balloons.world; $SHELL'" --tab --title="Driver" --command="bash -c 'cd ~; roslaunch continuous_driver continuous_driver.launch; $SHELL'"
