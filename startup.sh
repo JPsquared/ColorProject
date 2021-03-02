2f08672f06778979839ca6a01640bdf10ac53c7a
JPsquared <jpjpost1@gmail.com>
2021-03-02 16:38:05 -0500

    Add another command to start the image feed from the bot

--- startup.sh
+++ startup.sh
@@ -3,3 +3,5 @@
 gnome-terminal --tab --command="bash -c 'cd ~/ColorProject; ls; $SHELL'" \
                --tab --command="bash -c 'cd ~; sleep 5; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/administrator/roblab_with_balloons.world; $SHELL'" \
                --tab --command="bash -c 'cd ~; sleep 6; roslaunch continuous_driver continuous_driver.launch; $SHELL'"
+
+gnome-terminal --command="bash -c 'cd ~; rosrun image_view image_view image:=/camera/rgb/image_raw;'"
