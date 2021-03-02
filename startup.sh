98048f21f306c27791c0a5be31bea6de9e5011e3
JPsquared <jpjpost1@gmail.com>
2021-03-02 15:52:03 -0500

    Added more to startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 echo "$SHELL"
+
+bash roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/administrator/roblab_with_balloons.world &
+
+echo "Gazebo launched?"
+read REPLY
+
+bash roslaunch continuous_driver continuous_driver.launch &
+
+echo "Continuous driver launched?"
+read REPLY
