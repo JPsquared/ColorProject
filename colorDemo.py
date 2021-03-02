4324b1f9e945bfee8c229d939078e2c215f4ed34
JPsquared <jpjpost1@gmail.com>
2021-03-02 16:30:26 -0500

    Work on colorDemo.py

--- colorDemo.py
+++ colorDemo.py
@@ -1,7 +1,10 @@
+import rospy
 from turtleAPI import robot
 
 
 if __name__ == "__main__":
+    rate = rospy.Rate(10)
     rbt = robot()
     print("This worked")
     image = rbt.getImage()
+    print(type(image))
