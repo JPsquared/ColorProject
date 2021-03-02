c142c4b62e4d2d274c6020bcc9e250e323f1ef05
JPsquared <jpjpost1@gmail.com>
2021-03-02 17:03:46 -0500

    Remove call to image_view

--- startup.sh
+++ startup.sh
@@ -3,5 +3,3 @@
 gnome-terminal --tab --command="bash -c 'cd ~/ColorProject; $SHELL'" \
                --tab --command="bash -c 'cd ~; sleep 5; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/administrator/roblab_with_balloons.world; $SHELL'" \
                --tab --command="bash -c 'cd ~; sleep 6; roslaunch continuous_driver continuous_driver.launch; $SHELL'"
-
-gnome-terminal --command="bash -c 'cd ~; rosrun image_view image_view image:=/camera/rgb/image_raw; $SHELL'"
