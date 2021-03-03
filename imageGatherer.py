3076ac05e7ac723e414fc67658b9aac54ce76a53
JPsquared <jpjpost1@gmail.com>
2021-03-02 21:21:34 -0500

    Add code to take pictures

--- imageGatherer.py
+++ imageGatherer.py
@@ -1,4 +1,5 @@
 import rospy
+import cv2
 from time import sleep
 from numpy import pi
 from turtleAPI import robot
@@ -7,8 +8,30 @@ from turtleAPI import robot
 if __name__ == "__main__":
     rbt = robot()
     rate = rospy.Rate(10)
+
+    # figure out when to stop turning
     startAngle = rbt.getAngle()
-    # rbt.drive(.2, 0)
+
+    # start turning
+    rbt.drive(.2, 0)
     sleep(1)
     currentAngle = rbt.getAngle()
+
+    # initialize cache
+    imageList = []
+
+    # start a loop to take pictures
     print(currentAngle)
+    while currentAngle[2] < pi/2:
+        sleep(1)
+        # take a picture and add it to the cache
+        imageList.append((currentAngle, rbt.getImage()))
+        currentAngle = rbt.getAngle()
+
+    # stop turning
+    rbt.stop()
+
+    # display all the cached images
+    for image in imageList:
+        cv2.imshow(image[0], image[1])
+        cv2.waitKey(0)
