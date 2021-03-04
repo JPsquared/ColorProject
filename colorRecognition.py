df4d17a2f739c72df50a2d37f7c83bdaba4bc2eb
JPsquared <jpjpost1@gmail.com>
2021-03-04 15:46:14 -0500

    Create colorRecognition.py

---
+++ colorRecognition.py
@@ -0,0 +1,11 @@
+import rospy
+import cv2
+from os import chdir, getcwd
+
+
+if __name__ == "__main__":
+    print(getcwd())
+    # want to use cv2's blob detector
+    # cd to TestPhotos
+    chdir("TestPhotos")
+    exit()
