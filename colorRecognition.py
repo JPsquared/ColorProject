5e168b43f8a22e761aab984859e4b6b925a630b8
JPsquared <jpjpost1@gmail.com>
2021-03-04 18:43:37 -0500

    Fix color filter parameters

--- colorRecognition.py
+++ colorRecognition.py
@@ -11,15 +11,16 @@ if __name__ == "__main__":
     chdir("TestPhotos")
     path = getcwd()
     fileArray = [f for f in listdir(path) if isfile(join(path, f))]
+
     # get the filename minus '...Test.jpg'
-    for filename in fileArray:
-        print(filename[:-8])
+    # for filename in fileArray:
+    #     print(filename[:-8])
 
     blue = cv2.imread("blueTest.jpg")
     # cv2.imshow('blueTest.jpg', blue)
     # cv2.waitKey(0)
     bluehsv = cv2.cvtColor(blue, cv2.COLOR_BGR2HSV)
-    bluemask = cv2.inRange(bluehsv, np.array([241, 15, 20]), np.array([300, 255, 235]))
+    bluemask = cv2.inRange(bluehsv, np.array([210, 15, 20]), np.array([250, 255, 235]))
     cv2.imshow('blue mask', bluemask)
     cv2.waitKey(0)
 
