59856a4eee9aaf18a7d90704c216541e9b8855fa
JPsquared <jpjpost1@gmail.com>
2021-03-04 18:36:23 -0500

    Work on colorRecognition.py

--- colorRecognition.py
+++ colorRecognition.py
@@ -1,5 +1,6 @@
 import rospy
 import cv2
+import numpy as np
 from os import chdir, getcwd, listdir
 from os.path import isfile, join
 
@@ -11,9 +12,21 @@ if __name__ == "__main__":
     path = getcwd()
     fileArray = [f for f in listdir(path) if isfile(join(path, f))]
     # get the filename minus '...Test.jpg'
-    print(fileArray[1][:-8])
+    for filename in fileArray:
+        print(filename[:-8])
 
     blue = cv2.imread("blueTest.jpg")
     # cv2.imshow('blueTest.jpg', blue)
     # cv2.waitKey(0)
+    bluehsv = cv2.cvtColor(blue, cv2.COLOR_BGR2HSV)
+    bluemask = cv2.inRange(bluehsv, np.array([241, 15, 20]), np.array([300, 255, 235]))
+    cv2.imshow('blue mask', bluemask)
+    cv2.waitkey(0)
+
+    # Set up detector with default parameters
+    detector = cv2.SimpleBlobDetector()
+
+    # Detect blobs
+    # keypoints = detector.detect(blue)
+
     exit()
