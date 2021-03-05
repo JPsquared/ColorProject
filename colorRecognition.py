c82a7a095ee8fe9091a4ad4eb4e3f42803cffa16
JPsquared <jpjpost1@gmail.com>
2021-03-04 19:51:26 -0500

    Work on colorRecognition.py

--- colorRecognition.py
+++ colorRecognition.py
@@ -6,7 +6,9 @@ from os.path import isfile, join
 
 # Define constants
 FILE_TO_TEST = "blueTest.jpg"
-
+# divide hue number by 2 to get equivalent cv2 hue number
+#    H       S       V
+# [0..179, 0..255, 0..255]
 LOWER_BLUE = np.array([105, 50, 20])
 UPPER_BLUE = np.array([125, 255, 235])
 LOWER_GREEN = np.array([35, 15, 20])
@@ -17,6 +19,7 @@ LOWER_YELLOW = np.array([])
 UPPER_YELLOW = np.array([])
 LOWER_MAGENTA = np.array([])
 UPPER_MAGENTA = np.array([])
+
 
 if __name__ == "__main__":
     # want to use cv2's blob detector
@@ -30,11 +33,7 @@ if __name__ == "__main__":
     #     print(filename[:-8])
 
     img = cv2.imread(FILE_TO_TEST)
-    cv2.imshow(FILE_TO_TEST, img)
-    cv2.waitKey(0)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    cv2.imshow('hsv', hsv)
-    cv2.waitKey(0)
     mask = cv2.inRange(hsv, LOWER_BLUE, UPPER_BLUE)
     cv2.imshow('mask', mask)
     cv2.waitKey(0)
