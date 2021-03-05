738d1f26f7f3cfafeb6e4b005a93cd477b13c683
JPsquared <jpjpost1@gmail.com>
2021-03-05 16:14:21 -0500

    Work on colorRecognition.py

--- colorRecognition.py
+++ colorRecognition.py
@@ -96,6 +96,7 @@ if __name__ == "__main__":
     cv2.imshow('Augmented', img)
     cv2.waitKey(0)
 
+    # not really necessary but the keypoint circle can be overlaid on top of the augmented image
     im_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0, 0, 255),
                                           cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     cv2.imshow("Keypoints", im_with_keypoints)
