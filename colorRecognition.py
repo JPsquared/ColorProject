8395531bb0b97655a0101c6e46d636fe0b34833d
JPsquared <jpjpost1@gmail.com>
2021-03-04 19:03:51 -0500

    Add constants at top of file. Fudge blue mask numbers.

--- colorRecognition.py
+++ colorRecognition.py
@@ -4,6 +4,17 @@ import numpy as np
 from os import chdir, getcwd, listdir
 from os.path import isfile, join
 
+# Define constants
+LOWER_BLUE = np.array([210, 50, 20])
+UPPER_BLUE = np.array([250, 255, 235])
+LOWER_GREEN = np.array([])
+UPPER_GREEN = np.array([])
+LOWER_RED = np.array([])
+UPPER_RED = np.array([])
+LOWER_YELLOW = np.array([])
+UPPER_YELLOW = np.array([])
+LOWER_MAGENTA = np.array([])
+UPPER_MAGENTA = np.array([])
 
 if __name__ == "__main__":
     # want to use cv2's blob detector
@@ -16,16 +27,16 @@ if __name__ == "__main__":
     # for filename in fileArray:
     #     print(filename[:-8])
 
-    blue = cv2.imread("blueTest.jpg")
+    blueImg = cv2.imread("blueTest.jpg")
     # cv2.imshow('blueTest.jpg', blue)
     # cv2.waitKey(0)
-    bluehsv = cv2.cvtColor(blue, cv2.COLOR_BGR2HSV)
-    bluemask = cv2.inRange(bluehsv, np.array([210, 15, 20]), np.array([250, 255, 235]))
+    bluehsv = cv2.cvtColor(blueImg, cv2.COLOR_BGR2HSV)
+    bluemask = cv2.inRange(bluehsv, LOWER_BLUE, UPPER_BLUE)
     cv2.imshow('blue mask', bluemask)
     cv2.waitKey(0)
 
     # Set up detector with default parameters
-    detector = cv2.SimpleBlobDetector()
+    # detector = cv2.SimpleBlobDetector()
 
     # Detect blobs
     # keypoints = detector.detect(blue)
