8cf96aa74a7fba3f466f622cf7aabf42a804f530
JPsquared <jpjpost1@gmail.com>
2021-03-05 09:42:20 -0500

    Work on colorRecognition.py

--- colorRecognition.py
+++ colorRecognition.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from os import chdir, getcwd, listdir
 from os.path import isfile, join
+from turtleAPI import robot
 
 # Define constants
 FILE_TO_TEST = "blueTest.jpg"
@@ -11,7 +12,7 @@ FILE_TO_TEST = "blueTest.jpg"
 # [0..179, 0..255, 0..255]
 LOWER_BLUE = np.array([105, 50, 20])
 UPPER_BLUE = np.array([125, 255, 235])
-LOWER_GREEN = np.array([35, 15, 20])
+LOWER_GREEN = np.array([35, 50, 20])
 UPPER_GREEN = np.array([80, 255, 235])
 LOWER_RED = np.array([])
 UPPER_RED = np.array([])
@@ -22,6 +23,9 @@ UPPER_MAGENTA = np.array([])
 
 
 if __name__ == "__main__":
+    rbt = robot()
+    rate = rospy.Rate(10)
+
     # want to use cv2's blob detector
     # cd to TestPhotos
     chdir("TestPhotos")
@@ -37,7 +41,9 @@ if __name__ == "__main__":
     mask = cv2.inRange(hsv, LOWER_BLUE, UPPER_BLUE)
     cv2.imshow('mask', mask)
     cv2.waitKey(0)
-    mask = cv2.dilate(mask, None, iterations=1)
+    kernel = np.ones((3, 3), np.uint8)
+    mask = cv2.erode(mask, kernel, iterations=1)
+    # mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, iterations=1)
     cv2.imshow('dilate', mask)
     cv2.waitKey(0)
 
@@ -60,14 +66,21 @@ if __name__ == "__main__":
 
     # Detect blobs
     reversemask = 255 - mask
+    cv2.imshow('reverse mask', reversemask)
+    cv2.waitKey(0)
     keypoints = detector.detect(reversemask)
     print(keypoints)
     for k in keypoints:
         print(k.angle)
         print(type(k.pt))
+        # k.pt is the center of the 'blob' in the image
         print(k.pt)
         print(k.response)
         print(k.size)
+
+    # depthimg = rbt.getDepth()
+    # print(depthimg.size)
+    # print(depthimg)
 
     im_with_keypoints = cv2.drawKeypoints(mask, keypoints, np.array([]), (0, 0, 255),
                                           cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
