d7bad063be53cf7f21ccaeb53e5f23d01fb4e068
JPsquared <jpjpost1@gmail.com>
2021-03-05 14:30:06 -0500

    Work on colorRecognition.py

--- colorRecognition.py
+++ colorRecognition.py
@@ -110,7 +110,8 @@ if __name__ == "__main__":
 
     cf = ColorFinder()
     img = cv2.imread(FILE_TO_TEST)
-    keypoints, mask = cf.findColorInImage(img, "blue")
+    goal_color = input("Color to find: ")
+    keypoints, mask = cf.findColorInImage(img, goal_color)
 
     im_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0, 0, 255),
                                           cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
@@ -119,7 +120,8 @@ if __name__ == "__main__":
 
     # Create augmented version of image
     # This works for blue
-    img[:, :, 0] = np.bitwise_or(img[:, :, 0], mask)
+    # img[:, :, 0] = np.bitwise_or(img[:, :, 0], mask)
+    img[:, :, [0, 2]] = np.bitwise_or(img[:, :, [0, 2]], mask)  # magenta?
     cv2.imshow('Augmented', img)
     cv2.waitKey(0)
 
