fe2f71e637e1df2b9a4752c583264f6a1d5a87aa
JPsquared <jpjpost1@gmail.com>
2021-03-04 19:15:49 -0500

    Work on colorRecognition.py

--- colorRecognition.py
+++ colorRecognition.py
@@ -7,8 +7,8 @@ from os.path import isfile, join
 # Define constants
 LOWER_BLUE = np.array([210, 50, 20])
 UPPER_BLUE = np.array([250, 255, 235])
-LOWER_GREEN = np.array([])
-UPPER_GREEN = np.array([])
+LOWER_GREEN = np.array([125, 150, 200])
+UPPER_GREEN = np.array([150, 255, 235])
 LOWER_RED = np.array([])
 UPPER_RED = np.array([])
 LOWER_YELLOW = np.array([])
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     bluehsv = cv2.cvtColor(blueImg, cv2.COLOR_BGR2HSV)
     cv2.imshow('blue hsv', bluehsv)
     cv2.waitKey(0)
-    bluemask = cv2.inRange(bluehsv, LOWER_BLUE, UPPER_BLUE)
+    bluemask = cv2.inRange(bluehsv, LOWER_GREEN, UPPER_GREEN)
     cv2.imshow('blue mask', bluemask)
     cv2.waitKey(0)
 
