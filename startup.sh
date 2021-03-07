3e1aa5e75eeabefa8d170e76e717f11e2a92c826
JPsquared <jpjpost1@gmail.com>
2021-03-07 10:03:05 -0500

    Add line to start up the 3d sensor

--- startup.sh
+++ startup.sh
@@ -2,4 +2,5 @@
 
 gnome-terminal --tab --command="bash -c 'cd ~/ColorProject; $SHELL'" \
                --tab --command="bash -c 'cd ~; source .bashrc; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/administrator/roblab_with_balloons.world; $SHELL'" \
-               --tab --command="bash -c 'cd ~; source .bashrc; roslaunch continuous_driver continuous_driver.launch; $SHELL'"
+               --tab --command="bash -c 'cd ~; source .bashrc; roslaunch continuous_driver continuous_driver.launch; $SHELL'" \
+               --tab --command="bash -c 'cd ~; source .bashrc; roslaunch turtlebot_bringup 3dsensor.launch; $SHELL'"
