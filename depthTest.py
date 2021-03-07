bb0a55ada4ebb1bc88cec5c512a46da1c82858e3
JPsquared <jpjpost1@gmail.com>
2021-03-06 20:17:45 -0500

    Work on depthTest.py

--- depthTest.py
+++ depthTest.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     rbt = robot()
     rate = rospy.Rate(10)
 
+    # might need to start up the 3d sensor on VM before this will work
     depth_image = rbt.getDepth()
     cv2.imshow("Depth", depth_image)
     cv2.waitKey(0)
