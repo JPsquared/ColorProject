cc48f727bcae916c155feeaffdfb01ba51a026e9
JPsquared <jpjpost1@gmail.com>
2021-03-09 15:34:33 -0500

    Remove 3dsensor command from startup.sh

--- startup.sh
+++ startup.sh
@@ -2,5 +2,4 @@
 
 gnome-terminal --tab --command="bash -c 'cd ~/ColorProject; $SHELL'" \
                --tab --command="bash -c 'cd ~; source .bashrc; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/administrator/roblab_with_balloons.world; $SHELL'" \
-               --tab --command="bash -c 'cd ~; source .bashrc; roslaunch continuous_driver continuous_driver.launch; $SHELL'" \
-               --tab --command="bash -c 'cd ~; source .bashrc; roslaunch turtlebot_bringup 3dsensor.launch; $SHELL'"
+               --tab --command="bash -c 'cd ~; source .bashrc; roslaunch continuous_driver continuous_driver.launch; $SHELL'"
